3f0bf65b22491c5af72b47913400a5d55f2bf8e3
nachovaldavildozo-rgb <nachovaldavildozo@gmail.com>
2026-04-09 21:44:11 -0400

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,9 @@
+const phone = "59164030721";
 
+let msg = "Pedido LOVA:%0A";
+
+cart.forEach(p => {
+  msg += `- ${p.type} (${p.size})%0A`;
+});
+
+window.open(`https://wa.me/${phone}?text=${msg}`);
