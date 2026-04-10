d9b00da1515bca9695150b4ee3ec6033afc4dc90
nachovaldavildozo-rgb <nachovaldavildozo@gmail.com>
2026-04-09 21:48:57 -0400

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -7,3 +7,29 @@ cart.forEach(p => {
 });
 
 window.open(`https://wa.me/${phone}?text=${msg}`);
+const container = document.getElementById("cartItems");
+
+if (container) {
+
+  let total = 0;
+
+  cart.forEach((p, index) => {
+
+    const div = document.createElement("div");
+
+    div.className = "card p-4";
+
+    div.innerHTML = `
+      <p><strong>Tipo:</strong> ${p.type}</p>
+      <p><strong>Talla:</strong> ${p.size}</p>
+      <p><strong>Precio:</strong> 100 Bs</p>
+      <button onclick="removeItem(${index})">❌ Eliminar</button>
+    `;
+
+    total += 100;
+
+    container.appendChild(div);
+  });
+
+  document.getElementById("total").innerText = "Total: " + total + " Bs";
+}
